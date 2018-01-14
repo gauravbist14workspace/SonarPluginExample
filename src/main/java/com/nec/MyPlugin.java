2c29c20b91ca38e2e39c8c7070957ce5cd1bc350
Gaurav.Bist <30426678+gauravbist14workspace@users.noreply.github.com>
2018-01-14 08:56:11 +0000

    Simple sonar plugin

---
+++ src/main/java/com/nec/MyPlugin.java
@@ -0,0 +1,11 @@
+package com.nec;
+
+import org.sonar.api.SonarPlugin;
+
+import java.util.List;
+
+public class MyPlugin extends SonarPlugin{
+	public List getExtensions() {
+        return null;
+    }
+}
